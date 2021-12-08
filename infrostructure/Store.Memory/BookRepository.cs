8bd63ac6e1842eee8b0a10c5e3cc250635f51bfd
max7 <max7@DESKTOP-DQ2AN4C>
2021-12-08 11:17:18 +0200

    add web progect and folder

--- infrostructure/Store.Memory/BookRepository.cs
+++ infrostructure/Store.Memory/BookRepository.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Linq;
 
 namespace Store.Memory
 {
     public class BookRepository : IBookRepository
     {
+        private readonly Book[] books = new[]
+        {
+
+            new Book(1,"Art Of Programmin"),
+
+            new Book(2,"Refactoring"),
+
+
+            new Book(3,"C Programming Language")
+
+
+
+        };
+
+        public Book[] GetAllByTite(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                         .ToArray();
+        }
     }
 }
